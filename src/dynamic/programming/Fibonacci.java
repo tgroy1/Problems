793b06c1e6d9b2667d7177ff6eb1bfdbc1a59d0c
Tiyas <tgroy1@gmail.com>
2021-08-04 01:15:59 +0530

    Modified the iteration index limit

--- src/dynamic/programming/Fibonacci.java
+++ src/dynamic/programming/Fibonacci.java
@@ -89,7 +89,7 @@ public class Fibonacci {
 		long first = 0;
 		long second = 1;
 		long third = 0;
-		for (int i = 3; i <= n+1; i++) {
+		for (int i = 2; i <= n; i++) {
 			third = first + second;
             first = second;
             second = third;
